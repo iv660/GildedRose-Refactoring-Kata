50fd702730e1a1bbf612dc6e3d17878b27a1b31b
Ilya Vikharev <ilya.vikharev@k-gorod.ru>
2022-07-21 11:41:52 +0300

    fix: fix approval test

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -7,226 +7,226 @@ describe("Gilded Rose", () => {
     expect(items[0].name).toBe("foo");
   });
 
-  it("should pass APPROVAL", () => {
+  it('should pass APPROVAL', () => {
     const expected = [
       [
-        { name: "+5 Dexterity Vest", sellIn: 10, quality: 20 },
-        { name: "Aged Brie", sellIn: 2, quality: 0 },
-        { name: "Elixir of the Mongoose", sellIn: 5, quality: 7 },
+        { name: "+5 Dexterity Vest", sellIn: 9, quality: 19 },
+        { name: "Aged Brie", sellIn: 1, quality: 1 },
+        { name: "Elixir of the Mongoose", sellIn: 4, quality: 6 },
         { name: "Sulfuras, Hand of Ragnaros", sellIn: 0, quality: 80 },
         { name: "Sulfuras, Hand of Ragnaros", sellIn: -1, quality: 80 },
         {
           name: "Backstage passes to a TAFKAL80ETC concert",
-          sellIn: 15,
-          quality: 20,
+          sellIn: 14,
+          quality: 21,
         },
         {
           name: "Backstage passes to a TAFKAL80ETC concert",
-          sellIn: 10,
-          quality: 49,
+          sellIn: 9,
+          quality: 50,
         },
         {
           name: "Backstage passes to a TAFKAL80ETC concert",
-          sellIn: 5,
-          quality: 49,
+          sellIn: 4,
+          quality: 50,
         },
       ],
       [
-        { name: "+5 Dexterity Vest", sellIn: 10, quality: 20 },
-        { name: "Aged Brie", sellIn: 2, quality: 0 },
-        { name: "Elixir of the Mongoose", sellIn: 5, quality: 7 },
+        { name: "+5 Dexterity Vest", sellIn: 8, quality: 18 },
+        { name: "Aged Brie", sellIn: 0, quality: 2 },
+        { name: "Elixir of the Mongoose", sellIn: 3, quality: 5 },
         { name: "Sulfuras, Hand of Ragnaros", sellIn: 0, quality: 80 },
         { name: "Sulfuras, Hand of Ragnaros", sellIn: -1, quality: 80 },
         {
           name: "Backstage passes to a TAFKAL80ETC concert",
-          sellIn: 15,
-          quality: 20,
+          sellIn: 13,
+          quality: 22,
         },
         {
           name: "Backstage passes to a TAFKAL80ETC concert",
-          sellIn: 10,
-          quality: 49,
+          sellIn: 8,
+          quality: 50,
         },
         {
           name: "Backstage passes to a TAFKAL80ETC concert",
-          sellIn: 5,
-          quality: 49,
+          sellIn: 3,
+          quality: 50,
         },
       ],
       [
-        { name: "+5 Dexterity Vest", sellIn: 10, quality: 20 },
-        { name: "Aged Brie", sellIn: 2, quality: 0 },
-        { name: "Elixir of the Mongoose", sellIn: 5, quality: 7 },
+        { name: "+5 Dexterity Vest", sellIn: 7, quality: 17 },
+        { name: "Aged Brie", sellIn: -1, quality: 4 },
+        { name: "Elixir of the Mongoose", sellIn: 2, quality: 4 },
         { name: "Sulfuras, Hand of Ragnaros", sellIn: 0, quality: 80 },
         { name: "Sulfuras, Hand of Ragnaros", sellIn: -1, quality: 80 },
         {
           name: "Backstage passes to a TAFKAL80ETC concert",
-          sellIn: 15,
-          quality: 20,
+          sellIn: 12,
+          quality: 23,
         },
         {
           name: "Backstage passes to a TAFKAL80ETC concert",
-          sellIn: 10,
-          quality: 49,
+          sellIn: 7,
+          quality: 50,
         },
         {
           name: "Backstage passes to a TAFKAL80ETC concert",
-          sellIn: 5,
-          quality: 49,
+          sellIn: 2,
+          quality: 50,
         },
       ],
       [
-        { name: "+5 Dexterity Vest", sellIn: 10, quality: 20 },
-        { name: "Aged Brie", sellIn: 2, quality: 0 },
-        { name: "Elixir of the Mongoose", sellIn: 5, quality: 7 },
+        { name: "+5 Dexterity Vest", sellIn: 6, quality: 16 },
+        { name: "Aged Brie", sellIn: -2, quality: 6 },
+        { name: "Elixir of the Mongoose", sellIn: 1, quality: 3 },
         { name: "Sulfuras, Hand of Ragnaros", sellIn: 0, quality: 80 },
         { name: "Sulfuras, Hand of Ragnaros", sellIn: -1, quality: 80 },
         {
           name: "Backstage passes to a TAFKAL80ETC concert",
-          sellIn: 15,
-          quality: 20,
+          sellIn: 11,
+          quality: 24,
         },
         {
           name: "Backstage passes to a TAFKAL80ETC concert",
-          sellIn: 10,
-          quality: 49,
+          sellIn: 6,
+          quality: 50,
         },
         {
           name: "Backstage passes to a TAFKAL80ETC concert",
-          sellIn: 5,
-          quality: 49,
+          sellIn: 1,
+          quality: 50,
         },
       ],
       [
-        { name: "+5 Dexterity Vest", sellIn: 10, quality: 20 },
-        { name: "Aged Brie", sellIn: 2, quality: 0 },
-        { name: "Elixir of the Mongoose", sellIn: 5, quality: 7 },
+        { name: "+5 Dexterity Vest", sellIn: 5, quality: 15 },
+        { name: "Aged Brie", sellIn: -3, quality: 8 },
+        { name: "Elixir of the Mongoose", sellIn: 0, quality: 2 },
         { name: "Sulfuras, Hand of Ragnaros", sellIn: 0, quality: 80 },
         { name: "Sulfuras, Hand of Ragnaros", sellIn: -1, quality: 80 },
         {
           name: "Backstage passes to a TAFKAL80ETC concert",
-          sellIn: 15,
-          quality: 20,
-        },
-        {
-          name: "Backstage passes to a TAFKAL80ETC concert",
           sellIn: 10,
-          quality: 49,
+          quality: 25,
         },
         {
           name: "Backstage passes to a TAFKAL80ETC concert",
           sellIn: 5,
-          quality: 49,
+          quality: 50,
+        },
+        {
+          name: "Backstage passes to a TAFKAL80ETC concert",
+          sellIn: 0,
+          quality: 50,
         },
       ],
       [
-        { name: "+5 Dexterity Vest", sellIn: 10, quality: 20 },
-        { name: "Aged Brie", sellIn: 2, quality: 0 },
-        { name: "Elixir of the Mongoose", sellIn: 5, quality: 7 },
+        { name: "+5 Dexterity Vest", sellIn: 4, quality: 14 },
+        { name: "Aged Brie", sellIn: -4, quality: 10 },
+        { name: "Elixir of the Mongoose", sellIn: -1, quality: 0 },
         { name: "Sulfuras, Hand of Ragnaros", sellIn: 0, quality: 80 },
         { name: "Sulfuras, Hand of Ragnaros", sellIn: -1, quality: 80 },
         {
           name: "Backstage passes to a TAFKAL80ETC concert",
-          sellIn: 15,
-          quality: 20,
+          sellIn: 9,
+          quality: 27,
         },
         {
           name: "Backstage passes to a TAFKAL80ETC concert",
-          sellIn: 10,
-          quality: 49,
+          sellIn: 4,
+          quality: 50,
         },
         {
           name: "Backstage passes to a TAFKAL80ETC concert",
-          sellIn: 5,
-          quality: 49,
+          sellIn: -1,
+          quality: 0,
         },
       ],
       [
-        { name: "+5 Dexterity Vest", sellIn: 10, quality: 20 },
-        { name: "Aged Brie", sellIn: 2, quality: 0 },
-        { name: "Elixir of the Mongoose", sellIn: 5, quality: 7 },
+        { name: "+5 Dexterity Vest", sellIn: 3, quality: 13 },
+        { name: "Aged Brie", sellIn: -5, quality: 12 },
+        { name: "Elixir of the Mongoose", sellIn: -2, quality: 0 },
         { name: "Sulfuras, Hand of Ragnaros", sellIn: 0, quality: 80 },
         { name: "Sulfuras, Hand of Ragnaros", sellIn: -1, quality: 80 },
         {
           name: "Backstage passes to a TAFKAL80ETC concert",
-          sellIn: 15,
-          quality: 20,
+          sellIn: 8,
+          quality: 29,
         },
         {
           name: "Backstage passes to a TAFKAL80ETC concert",
-          sellIn: 10,
-          quality: 49,
+          sellIn: 3,
+          quality: 50,
         },
         {
           name: "Backstage passes to a TAFKAL80ETC concert",
-          sellIn: 5,
-          quality: 49,
+          sellIn: -2,
+          quality: 0,
         },
       ],
       [
-        { name: "+5 Dexterity Vest", sellIn: 10, quality: 20 },
-        { name: "Aged Brie", sellIn: 2, quality: 0 },
-        { name: "Elixir of the Mongoose", sellIn: 5, quality: 7 },
+        { name: "+5 Dexterity Vest", sellIn: 2, quality: 12 },
+        { name: "Aged Brie", sellIn: -6, quality: 14 },
+        { name: "Elixir of the Mongoose", sellIn: -3, quality: 0 },
         { name: "Sulfuras, Hand of Ragnaros", sellIn: 0, quality: 80 },
         { name: "Sulfuras, Hand of Ragnaros", sellIn: -1, quality: 80 },
         {
           name: "Backstage passes to a TAFKAL80ETC concert",
-          sellIn: 15,
-          quality: 20,
+          sellIn: 7,
+          quality: 31,
         },
         {
           name: "Backstage passes to a TAFKAL80ETC concert",
-          sellIn: 10,
-          quality: 49,
+          sellIn: 2,
+          quality: 50,
         },
         {
           name: "Backstage passes to a TAFKAL80ETC concert",
-          sellIn: 5,
-          quality: 49,
+          sellIn: -3,
+          quality: 0,
         },
       ],
       [
-        { name: "+5 Dexterity Vest", sellIn: 10, quality: 20 },
-        { name: "Aged Brie", sellIn: 2, quality: 0 },
-        { name: "Elixir of the Mongoose", sellIn: 5, quality: 7 },
+        { name: "+5 Dexterity Vest", sellIn: 1, quality: 11 },
+        { name: "Aged Brie", sellIn: -7, quality: 16 },
+        { name: "Elixir of the Mongoose", sellIn: -4, quality: 0 },
         { name: "Sulfuras, Hand of Ragnaros", sellIn: 0, quality: 80 },
         { name: "Sulfuras, Hand of Ragnaros", sellIn: -1, quality: 80 },
         {
           name: "Backstage passes to a TAFKAL80ETC concert",
-          sellIn: 15,
-          quality: 20,
+          sellIn: 6,
+          quality: 33,
         },
         {
           name: "Backstage passes to a TAFKAL80ETC concert",
-          sellIn: 10,
-          quality: 49,
+          sellIn: 1,
+          quality: 50,
         },
         {
           name: "Backstage passes to a TAFKAL80ETC concert",
-          sellIn: 5,
-          quality: 49,
+          sellIn: -4,
+          quality: 0,
         },
       ],
       [
-        { name: "+5 Dexterity Vest", sellIn: 10, quality: 20 },
-        { name: "Aged Brie", sellIn: 2, quality: 0 },
-        { name: "Elixir of the Mongoose", sellIn: 5, quality: 7 },
+        { name: "+5 Dexterity Vest", sellIn: 0, quality: 10 },
+        { name: "Aged Brie", sellIn: -8, quality: 18 },
+        { name: "Elixir of the Mongoose", sellIn: -5, quality: 0 },
         { name: "Sulfuras, Hand of Ragnaros", sellIn: 0, quality: 80 },
         { name: "Sulfuras, Hand of Ragnaros", sellIn: -1, quality: 80 },
         {
           name: "Backstage passes to a TAFKAL80ETC concert",
-          sellIn: 15,
-          quality: 20,
-        },
-        {
-          name: "Backstage passes to a TAFKAL80ETC concert",
-          sellIn: 10,
-          quality: 49,
-        },
-        {
-          name: "Backstage passes to a TAFKAL80ETC concert",
           sellIn: 5,
-          quality: 49,
+          quality: 35,
+        },
+        {
+          name: "Backstage passes to a TAFKAL80ETC concert",
+          sellIn: 0,
+          quality: 50,
+        },
+        {
+          name: "Backstage passes to a TAFKAL80ETC concert",
+          sellIn: -5,
+          quality: 0,
         },
       ],
     ];
@@ -241,9 +241,9 @@ describe("Gilded Rose", () => {
       new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
       new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
       // this conjured item does not work properly yet
-      // new Item("Conjured Mana Cake", 3, 6),
+      // new Item("Conjured Mana Cake", 3, 6)
     ];
-
+    
     const gildedRose = new GildedRose(items);
 
     const days: number = 10;
@@ -251,7 +251,10 @@ describe("Gilded Rose", () => {
     const results: Item[][] = [];
 
     for (let day = 0; day < days; day++) {
+      gildedRose.updateQuality();
+
       results.push([]);
+
       items.forEach((item) => {
         const value = {
           name: item.name,
@@ -264,5 +267,5 @@ describe("Gilded Rose", () => {
 
     console.log(JSON.stringify(results));
     expect(results).toEqual(expected);
-  });
+  })
 });
